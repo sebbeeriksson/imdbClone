c6e0dca41136c526665bb7f5e38f44e808ca2a15
sebastianErixon <sebastian.eriksson@homy.digital>
2024-10-16 00:40:41 +0200

    Add project files.

---
+++ Hellow/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+
+namespace Hellow
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hi my friend");
+        }
+    }
+}
