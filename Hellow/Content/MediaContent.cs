7dd93d1cbecf3b6c7b4529f384bd8f7f67eebf75
sebastianErixon <sebastian.eriksson@homy.digital>
2024-10-16 02:03:52 +0200

    Added some classes

---
+++ Hellow/Content/MediaContent.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hellow
+{
+    abstract class MediaContent
+    {
+        public int id;
+        public string name;
+        public string description;
+        private double rating;
+         
+    }
+}
---
+++ Hellow/Imdb.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hellow
+{
+    internal class Imdb
+    {
+
+        public int Id { get; set; }
+        
+        private string[] listOfAllmovies;
+        private string[] listofAllseries;
+
+    }
+}
